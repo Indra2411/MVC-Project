47acf14f2c2940d11a8c5a1188f11b20485b711b
indrajeet <patilindrajeet2411@gmail.com>
2024-04-27 09:11:44 +0530

    initial commit

---
+++ connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectMongo = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/prod")
+    .then(() => console.log("connected to mongodb.."))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectMongo;
